0df6ad49f519d7dac44c779e7c91513694116552
hakim_gaku <hakimbmx88@gmail.com>
2026-06-06 08:16:39 +0100

    Replace broken icon fonts with unicode characters

--- app/+html.tsx
+++ app/+html.tsx
@@ -8,7 +8,21 @@ export default function Html({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        
+
+        {/* 🛠️ الحل الجذري والنهائي: تحميل الخطوط بدون تعارض */}
+        <style dangerouslySetInnerHTML={{ __html: `
+          @font-face {
+            font-family: 'MaterialIcons';
+            src: url('https://cdn.jsdelivr.net/npm/react-native-vector-icons@10.0.3/Fonts/MaterialIcons.ttf') format('truetype');
+            font-display: block;
+          }
+          @font-face {
+            font-family: 'FontAwesome';
+            src: url('https://cdn.jsdelivr.net/npm/react-native-vector-icons@10.0.3/Fonts/FontAwesome.ttf') format('truetype');
+            font-display: block;
+          }
+        `}} />
+
         <ScrollViewStyleReset />
       </head>
       <body>{children}</body>
