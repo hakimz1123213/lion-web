2105a77872783f2790664b85a8ff78c55507f3a8
hakim_gaku <hakimbmx88@gmail.com>
2026-06-06 12:21:19 +0100

    Replace broken icon fonts with unicode characters

--- app/+html.tsx
+++ app/+html.tsx
@@ -9,16 +9,30 @@ export default function Html({ children }: { children: React.ReactNode }) {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-        {/* 🛠️ الحل الجذري والنهائي: تحميل الخطوط بدون تعارض */}
         <style dangerouslySetInnerHTML={{ __html: `
           @font-face {
+            font-family: 'Ionicons';
+            src: url('https://cdn.jsdelivr.net/npm/@expo/vector-icons@14.0.0/build/vendor/react-native-vector-icons/Fonts/Ionicons.ttf') format('truetype');
+            font-display: block;
+          }
+          @font-face {
             font-family: 'MaterialIcons';
-            src: url('https://cdn.jsdelivr.net/npm/react-native-vector-icons@10.0.3/Fonts/MaterialIcons.ttf') format('truetype');
+            src: url('https://cdn.jsdelivr.net/npm/@expo/vector-icons@14.0.0/build/vendor/react-native-vector-icons/Fonts/MaterialIcons.ttf') format('truetype');
+            font-display: block;
+          }
+          @font-face {
+            font-family: 'AntDesign';
+            src: url('https://cdn.jsdelivr.net/npm/@expo/vector-icons@14.0.0/build/vendor/react-native-vector-icons/Fonts/AntDesign.ttf') format('truetype');
             font-display: block;
           }
           @font-face {
             font-family: 'FontAwesome';
-            src: url('https://cdn.jsdelivr.net/npm/react-native-vector-icons@10.0.3/Fonts/FontAwesome.ttf') format('truetype');
+            src: url('https://cdn.jsdelivr.net/npm/@expo/vector-icons@14.0.0/build/vendor/react-native-vector-icons/Fonts/FontAwesome.ttf') format('truetype');
+            font-display: block;
+          }
+          @font-face {
+            font-family: 'Feather';
+            src: url('https://cdn.jsdelivr.net/npm/@expo/vector-icons@14.0.0/build/vendor/react-native-vector-icons/Fonts/Feather.ttf') format('truetype');
             font-display: block;
           }
         `}} />
