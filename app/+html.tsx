3c6bba9279ef131b02ff6b3871c33bfd334944a6
hakim_gaku <hakimbmx88@gmail.com>
2026-06-06 06:56:58 +0100

    Fix final web font icons links integration

--- app/+html.tsx
+++ app/+html.tsx
@@ -5,17 +5,10 @@ export default function Html({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-
-        {/* 🛠️ حقن الأيقونات والخطوط الرسمية هنا عشان الـ Expo يدمجهم بالسيف */}
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/4.5.6/css/ionicons.min.css" />
-
-        <ScrollViewStyleReset />
-      </head>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  
+  </head>
       <body>{children}</body>
     </html>
   );
